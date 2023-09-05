144f078cfc68e2a1128490c35a60cd5421701d34
Brett Dickenson <46332464+brettdickenson1@users.noreply.github.com>
2023-09-05 15:49:14 +0100

    add-todo-logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input className="input__box" type="input" placeholder="enter a task" />
-      <button className="input__submit" type="submit">
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+        type="input"
+        placeholder="enter a task"
+      />
+      <button className="input_submit" type="submit">
         Go
       </button>
     </form>
